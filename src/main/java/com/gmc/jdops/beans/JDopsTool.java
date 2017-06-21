a3ee406f04fecd2f1e76d07ac2ccb99169574719
Vyas <vyas.mohan@gmail.com>
2017-06-21 09:17:06 -0500

    jl

--- src/main/java/com/gmc/jdops/beans/JDopsTool.java
+++ src/main/java/com/gmc/jdops/beans/JDopsTool.java
@@ -8,11 +8,26 @@ public class JDopsTool implements Serializable{
   public String purpose;
   public String comments;
   public boolean isApproved;
+  public final String beep="DO";
+  private String desc;
   public JDopsTool(String id, String name, String purpose, String comments, boolean isApproved){
     this.id = id;
     this.name = name;
     this.purpose = purpose;
     this.comments = comments;
     this.isApproved = isApproved;
+  }
+
+  public String test(){
+    String beep = "myID";
+    return beep;
+  }
+
+  public void setName(String name){
+    this.name = name;
+  }
+
+  public void setDesc(String desc){
+    this.desc = desc;
   }
 } 
